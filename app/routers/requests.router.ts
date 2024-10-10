9ceb4caafde88820f4ff1b6045a54b0fa38d3538
AntoineLetailleur <ant.letailleur@gmail.com>
2024-10-10 19:02:54 +0200

    Add middleware to secure endpoint on requests.router

--- app/routers/requests.router.ts
+++ app/routers/requests.router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import requestsController from "../controllers/requests.controller";
+import usersController from "../controllers/users.controller";
 
 
 const router = express.Router();
 
-router.post("/request", requestsController.addNewRequest);
+router.post("/request", usersController.validateRequest(['USER', 'DOCTOR', 'ADMIN']), requestsController.addNewRequest);
 
 export default router;
     
